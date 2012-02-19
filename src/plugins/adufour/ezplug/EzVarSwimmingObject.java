a254818e308edca2ba063b57e384ad916ee1b01f
Alexandre Dufour <adufour@pasteur.fr>
2012-02-19 23:26:08 +0100

    Updated EzVarSwimmingObject to the updated ConstraintByType class (Vars library)

--- src/plugins/adufour/ezplug/EzVarSwimmingObject.java
+++ src/plugins/adufour/ezplug/EzVarSwimmingObject.java
@@ -1,13 +1,14 @@
 package plugins.adufour.ezplug;
 
 import icy.swimmingPool.SwimmingObject;
+import plugins.adufour.vars.lang.ConstraintByType;
 import plugins.adufour.vars.lang.VarSwimmingObject;
 
 public class EzVarSwimmingObject<T> extends EzVar<SwimmingObject>
 {
 	public EzVarSwimmingObject(String varName)
 	{
-		super(new VarSwimmingObject(varName, null), new VarSwimmingObject.TypeConstraint<T>());
+		super(new VarSwimmingObject(varName, null), new ConstraintByType<T>());
 	}
 		
 	/**
