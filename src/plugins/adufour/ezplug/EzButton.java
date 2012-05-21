a6e02190aec54a942b3e3030eab9cb80b2cd6d0d
Alexandre Dufour <adufour@pasteur.fr>
2012-05-21 16:40:37 +0200

    new: EzButton.setText() (+ minor threading issue)

--- src/plugins/adufour/ezplug/EzButton.java
+++ src/plugins/adufour/ezplug/EzButton.java
@@ -12,65 +12,84 @@ import javax.swing.JButton;
 import javax.swing.SwingUtilities;
 
 /**
- * Class defining a button for use with EzPlugs. An EzButton adopts the same layout as other EzComponents, and fires an
- * action listener whenever it is clicked from the interface.
+ * Class defining a button for use with EzPlugs. An EzButton adopts the same layout as other
+ * EzComponents, and fires an action listener whenever it is clicked from the interface.
  * 
  * @author Alexandre Dufour
  * 
  */
 public class EzButton extends EzComponent
 {
-    private JButton jButton;
-
-    /**
-     * Creates a new EzButton with given title and action listener (i.e. the method which will be called when the button
-     * is clicked)
-     * 
-     * @param title
-     *            the button title
-     * @param listener
-     *            the listener which will be called when the button is clicked
-     */
-    public EzButton(final String title, final ActionListener listener)
-    {
-        super(title);
-
-        ThreadUtil.invoke(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                jButton = new JButton(title);
-                jButton.addActionListener(listener);
-            }
-        }, !SwingUtilities.isEventDispatchThread());
-    }
-
-    @Override
-    public void addTo(Container container)
-    {
-        GridBagLayout gridbag = (GridBagLayout) container.getLayout();
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(2, 5, 2, 5);
-
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-
-//        jButton.setFocusable(false);
-        gridbag.setConstraints(jButton, gbc);
-        container.add(jButton);
-    }
-
-    @Override
-	public void setToolTipText(String text)
+	private JButton	jButton;
+	
+	/**
+	 * Creates a new EzButton with given title and action listener (i.e. the method which will be
+	 * called when the button is clicked)
+	 * 
+	 * @param title
+	 *            the button title
+	 * @param listener
+	 *            the listener which will be called when the button is clicked
+	 */
+	public EzButton(final String title, final ActionListener listener)
 	{
-		jButton.setToolTipText(text);
+		super(title);
+		
+		ThreadUtil.invoke(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				jButton = new JButton(title);
+				jButton.addActionListener(listener);
+			}
+		}, !SwingUtilities.isEventDispatchThread());
 	}
-
+	
 	@Override
-    public void dispose()
-    {
-        jButton = null;
-        super.dispose();
-    }
+	public void addTo(Container container)
+	{
+		GridBagLayout gridbag = (GridBagLayout) container.getLayout();
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.insets = new Insets(2, 5, 2, 5);
+		
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		
+		// jButton.setFocusable(false);
+		gridbag.setConstraints(jButton, gbc);
+		container.add(jButton);
+	}
+	
+	@Override
+	public void setToolTipText(final String text)
+	{
+		ThreadUtil.invoke(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				jButton.setToolTipText(text);
+			}
+		}, !SwingUtilities.isEventDispatchThread());
+	}
+	
+	public void setText(final String text)
+	{
+		ThreadUtil.invoke(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				jButton.setText(text);
+			}
+		}, !SwingUtilities.isEventDispatchThread());
+	}
+	
+	@Override
+	public void dispose()
+	{
+		jButton = null;
+		super.dispose();
+	}
 }
