28aaa9926ecb8973e4e2ba87296b6236dbffe2bf
Alexandre Dufour <adufour@pasteur.fr>
2012-09-11 00:00:59 +0200

    EzVarDimensionPicker now based on Var<Sequence> instead of EzVarSequence

--- src/plugins/adufour/ezplug/EzVarDimensionPicker.java
+++ src/plugins/adufour/ezplug/EzVarDimensionPicker.java
@@ -2,35 +2,95 @@ package plugins.adufour.ezplug;
 
 import icy.sequence.DimensionId;
 import icy.sequence.Sequence;
+import plugins.adufour.vars.lang.Var;
+import plugins.adufour.vars.util.VarListener;
 
-public class EzVarDimensionPicker extends EzVarInteger implements EzVarListener<Sequence>
+public class EzVarDimensionPicker extends EzVarInteger
 {
-    final EzVarSequence s;
-    final DimensionId   dim;
-
-    public EzVarDimensionPicker(String varName, DimensionId dim, EzVarSequence sequence)
+    private final class SequenceListener implements VarListener<Sequence>
     {
-        super(varName, 0, 0, 1);
-        s = sequence;
-        this.dim = dim;
-        s.addVarChangeListener(this);
+        @Override
+        public void valueChanged(Var<Sequence> source, Sequence oldValue, Sequence newValue)
+        {
+            setValue(0);
+
+            if (newValue == null)
+            {
+                setVisible(false);
+            }
+            else
+            {
+                int size = getSize(newValue, dim);
+                setVisible(size > 1);
+                setMaxValue(size - 1);
+            }
+        }
+
+        @Override
+        public void referenceChanged(Var<Sequence> source, Var<? extends Sequence> oldReference, Var<? extends Sequence> newReference)
+        {
+        }
     }
 
-    @Override
-    public void variableChanged(EzVar<Sequence> source, Sequence newValue)
-    {
-        setValue(0);
+    final Var<Sequence>    s;
+    final DimensionId      dim;
+    final SequenceListener listener;
 
-        if (newValue == null)
-        {
-            setVisible(false);
-        }
-        else
-        {
-            int size = getSize(newValue, dim);
-            setVisible(size > 1);
-            setMaxValue(size - 1);
-        }
+    /**
+     * Constructs a new selector for the specified dimension. By default, <code>-1</code> is not a
+     * valid value.
+     * 
+     * @param varName
+     *            the variable name
+     * @param dim
+     *            the dimension to create a selector for
+     * @param sequence
+     *            the sequence variable to listen to
+     */
+    public EzVarDimensionPicker(String varName, DimensionId dim, EzVarSequence sequence)
+    {
+        this(varName, dim, sequence.variable);
+    }
+
+    /**
+     * Constructs a new selector for the specified dimension
+     * 
+     * @param varName
+     *            the variable name
+     * @param dim
+     *            the dimension to create a selector for
+     * @param sequence
+     *            the sequence variable to listen to
+     * @param allowAll
+     *            true if <code>-1</code> is a valid value, i.e. all values are selected (e.g. -1
+     *            for dimension C indicates that all channels should be processed). If false, the
+     *            minimum selector value will be <code>0</code>
+     */
+    public EzVarDimensionPicker(String varName, DimensionId dim, Var<Sequence> sequence)
+    {
+        this(varName, dim, sequence, false);
+    }
+
+    /**
+     * Constructs a new selector for the specified dimension
+     * 
+     * @param varName
+     *            the variable name
+     * @param dim
+     *            the dimension to create a selector for
+     * @param sequence
+     *            the sequence variable to listen to
+     * @param allowAll
+     *            true if <code>-1</code> is a valid value, i.e. all values are selected (e.g. -1
+     *            for dimension C indicates that all channels should be processed). If false, the
+     *            minimum selector value will be <code>0</code>
+     */
+    public EzVarDimensionPicker(String varName, DimensionId dim, Var<Sequence> sequence, boolean allowAll)
+    {
+        super(varName, allowAll ? -1 : 0, allowAll ? -1 : 0, 0, 1);
+        s = sequence;
+        this.dim = dim;
+        s.addListener(listener = new SequenceListener());
     }
 
     private int getSize(Sequence s, DimensionId dim)
@@ -55,7 +115,7 @@ public class EzVarDimensionPicker extends EzVarInteger implements EzVarListener<
     @Override
     protected void dispose()
     {
-        s.removeVarChangeListener(this);
+        s.removeListener(listener);
         super.dispose();
     }
 }
