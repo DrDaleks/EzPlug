19e3184993fd761768644b32f2f8ac8b6860aa29
Alexandre Dufour <adufour@pasteur.fr>
2012-09-27 23:37:34 +0200

    PluginChooser uses a JXHeader component inside its combo box

--- src/plugins/adufour/vars/gui/swing/PluginChooser.java
+++ src/plugins/adufour/vars/gui/swing/PluginChooser.java
@@ -4,11 +4,17 @@ import icy.plugin.PluginDescriptor;
 import icy.plugin.PluginLoader;
 import icy.plugin.abstract_.Plugin;
 
+import java.awt.Component;
+
 import javax.swing.ComboBoxModel;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
 import javax.swing.event.ListDataListener;
 
-import org.jdesktop.swingx.JXComboBox;
+import org.jdesktop.swingx.JXHeader;
 
 import plugins.adufour.vars.lang.Var;
 import plugins.adufour.vars.lang.VarPlugin;
@@ -19,19 +25,19 @@ public class PluginChooser<P extends Plugin> extends SwingVarEditor<PluginDescri
     {
         super(variable);
     }
-    
+
     @Override
     protected JComponent createEditorComponent()
     {
-        JXComboBox combo = new JXComboBox();
-        
+        JComboBox combo = new JComboBox();
+
         combo.setModel(new ComboBoxModel()
         {
             @Override
             public void removeListDataListener(ListDataListener l)
             {
             }
-            
+
             @Override
             public int getSize()
             {
@@ -39,7 +45,7 @@ public class PluginChooser<P extends Plugin> extends SwingVarEditor<PluginDescri
                 Class<P> pluginType = ((VarPlugin<P>) variable).pluginType;
                 return PluginLoader.getPlugins(pluginType).size();
             }
-            
+
             @Override
             public Object getElementAt(int index)
             {
@@ -47,47 +53,61 @@ public class PluginChooser<P extends Plugin> extends SwingVarEditor<PluginDescri
                 Class<P> pluginType = ((VarPlugin<P>) variable).pluginType;
                 return PluginLoader.getPlugins(pluginType).get(index);
             }
-            
+
             @Override
             public void addListDataListener(ListDataListener l)
             {
             }
-            
+
             @Override
             public void setSelectedItem(Object anItem)
             {
                 variable.setValue((PluginDescriptor) anItem);
             }
-            
+
             @Override
             public Object getSelectedItem()
             {
                 return variable.getValue();
             }
         });
-        
+
+        if (combo.getModel().getSize() > 1) combo.setSelectedIndex(0);
+
+        combo.setRenderer(new ListCellRenderer()
+        {
+            @Override
+            public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus)
+            {
+                if (value == null) return new JLabel("no selection");
+                PluginDescriptor desc = (PluginDescriptor) value;
+                JXHeader header = new JXHeader(desc.getName(), desc.getDescription());
+                return header;
+            }
+        });
+
         return combo;
     }
-    
+
     @Override
     protected void activateListeners()
     {
         // TODO Auto-generated method stub
-        
+
     }
-    
+
     @Override
     protected void deactivateListeners()
     {
         // TODO Auto-generated method stub
-        
+
     }
-    
+
     @Override
     protected void updateInterfaceValue()
     {
         // TODO Auto-generated method stub
-        
+
     }
-    
+
 }
