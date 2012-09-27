9df2d16f54b97178521aaa2532cb786a627f80d2
Alexandre Dufour <adufour@pasteur.fr>
2012-09-27 20:06:55 +0200

    Fixed display in VarString/EzVarText

--- src/plugins/adufour/ezplug/EzVarText.java
+++ src/plugins/adufour/ezplug/EzVarText.java
@@ -18,9 +18,50 @@ public class EzVarText extends EzVar<String>
      */
     public EzVarText(String varName)
     {
-        super(new VarString(varName, ""), null);
+        this(varName, 1);
     }
-
+    
+    /**
+     * Creates a new Text variable
+     * 
+     * @param varName
+     *            the name of the variable (as it will appear on the interface)
+     * @param multiLine
+     *            allow multiple lines
+     */
+    public EzVarText(String varName, int nbLines)
+    {
+        this(varName, "", nbLines);
+    }
+    
+    /**
+     * Creates a new Text variable
+     * 
+     * @param varName
+     *            the name of the variable (as it will appear on the interface)
+     * @param defaultText
+     *            the default text to enter (the number of lines will be derived from this text)
+     */
+    public EzVarText(String varName, String defaultText)
+    {
+        this(varName, defaultText, defaultText.split("\n").length);
+    }
+    
+    /**
+     * Creates a new Text variable
+     * 
+     * @param varName
+     *            the name of the variable (as it will appear on the interface)
+     * @param defaultText
+     *            the default text to enter
+     * @param nbLines
+     *            the number of lines to display on the interface
+     */
+    public EzVarText(String varName, String defaultText, int nbLines)
+    {
+        super(new VarString(varName, defaultText, nbLines), null);
+    }
+    
     /**
      * Creates a new Text input variable with a list of default values
      * 
@@ -29,14 +70,14 @@ public class EzVarText extends EzVar<String>
      * @param defaultValues
      *            the list of default values the user may choose from
      * @param allowUserInput
-     *            set to true if the user may input text manually instead of selecting a default value, or false to
-     *            forbid user input
+     *            set to true if the user may input text manually instead of selecting a default
+     *            value, or false to forbid user input
      */
     public EzVarText(String varName, String[] defaultValues, Boolean allowUserInput)
     {
         this(varName, defaultValues, 0, allowUserInput);
     }
-
+    
     /**
      * Creates a new Text input variable with a list of default values and a default selection
      * 
@@ -47,8 +88,8 @@ public class EzVarText extends EzVar<String>
      * @param defaultValueIndex
      *            the index of the default selected value
      * @param allowUserInput
-     *            set to true if the user may input text manually instead of selecting a default value, or false to
-     *            forbid user input
+     *            set to true if the user may input text manually instead of selecting a default
+     *            value, or false to forbid user input
      */
     public EzVarText(String varName, String[] defaultValues, int defaultValueIndex, Boolean allowUserInput)
     {
--- src/plugins/adufour/vars/lang/VarString.java
+++ src/plugins/adufour/vars/lang/VarString.java
@@ -1,26 +1,49 @@
 package plugins.adufour.vars.lang;
 
 import plugins.adufour.vars.gui.VarEditor;
+import plugins.adufour.vars.gui.swing.TextArea;
 import plugins.adufour.vars.gui.swing.TextField;
 
 public class VarString extends Var<String>
 {
-	public VarString(String name, String defaultValue)
-	{
-		super(name, String.class, defaultValue);
-	}
-	
-	@Override
-	public String parse(String s)
-	{
-		return s;
-	}
-	
-	@Override
-	public VarEditor<String> createVarEditor()
-	{
-		if (getDefaultEditorModel() == null) return new TextField<String>(this);
-		
-		return super.createVarEditor();
-	}
+    private final int nbLines;
+    
+    /**
+     * Creates a new text variable with a default text field editor
+     * 
+     * @param name
+     * @param defaultValue
+     */
+    public VarString(String name, String defaultValue)
+    {
+        this(name, defaultValue, 1);
+    }
+    
+    /**
+     * Creates a new text variable with a default text field editor
+     * 
+     * @param name
+     * @param defaultValue
+     * @param multiLine
+     *            allow multiple lines
+     */
+    public VarString(String name, String defaultValue, int nbLines)
+    {
+        super(name, String.class, defaultValue);
+        this.nbLines = nbLines;
+    }
+    
+    @Override
+    public String parse(String s)
+    {
+        return s;
+    }
+    
+    @Override
+    public VarEditor<String> createVarEditor()
+    {
+        if (getDefaultEditorModel() == null) return nbLines > 1 ? new TextArea<String>(this, nbLines) : new TextField<String>(this);
+        
+        return super.createVarEditor();
+    }
 }
