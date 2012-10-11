2860640ca880f4898c0a2a75adfad8c32b2958fd
Alexandre Dufour <adufour@pasteur.fr>
2012-10-11 17:38:04 +0200

    new custom constructor for TypeSelectionModel, allows any combination of input types

--- src/plugins/adufour/vars/gui/model/TypeSelectionModel.java
+++ src/plugins/adufour/vars/gui/model/TypeSelectionModel.java
@@ -1,11 +1,26 @@
 package plugins.adufour.vars.gui.model;
 
+import icy.sequence.Sequence;
+
 import java.io.File;
 
 public class TypeSelectionModel extends ValueSelectionModel<Class<?>>
 {
-    public TypeSelectionModel()
-    {
-        super(new Class<?>[] { null, Integer.class, Double.class, int[].class, double[].class, String.class, File.class, File[].class });
-    }
+	/**
+	 * Create a new type selection model with the most popular types
+	 */
+	public TypeSelectionModel()
+	{
+		super(new Class<?>[] { null, Sequence.class, Integer.class, Double.class, int[].class, double[].class, String.class, File.class, File[].class });
+	}
+
+	/**
+	 * Creates a new custom type selection model with the specified types
+	 * 
+	 * @param classes
+	 */
+	public TypeSelectionModel(Class<?>... classes)
+	{
+		super(classes);
+	}
 }
