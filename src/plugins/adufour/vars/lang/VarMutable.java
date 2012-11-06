baf47b2a668c181ae00f39e2fc6adccc4affbe27
Alexandre Dufour <adufour@pasteur.fr>
2012-11-06 19:36:44 +0100

    Throw an error when changing the type of a linked mutable variable (WARNING: release together with Blocks commit 9a6c580)

--- src/plugins/adufour/vars/lang/VarMutable.java
+++ src/plugins/adufour/vars/lang/VarMutable.java
@@ -19,111 +19,113 @@ import plugins.adufour.vars.util.TypeChangeListener;
 @SuppressWarnings("rawtypes")
 public class VarMutable extends Var implements MutableType
 {
-	private final ArrayList<TypeChangeListener>	listeners	= new ArrayList<TypeChangeListener>();
-
-	/**
-	 * Constructs a new mutable variable with specified name and type. The default value for mutable
-	 * variables is always null, in order to be reset properly
-	 * 
-	 * @param name
-	 * @param initialType
-	 *            the type of variable to handle (this can be changed via the
-	 *            {@link #setType(Class)} method
-	 */
-	@SuppressWarnings("unchecked")
-	public VarMutable(String name, Class<?> initialType)
-	{
-		super(name, initialType, null);
-	}
-
-	@Override
-	public VarEditor createVarEditor()
-	{
-		if (getDefaultEditorModel() instanceof TypeSelectionModel) return new TypeChooser(this);
-
-		return super.createVarEditor();
-	}
-
-	public void addTypeChangeListener(TypeChangeListener listener)
-	{
-		listeners.add(listener);
-	}
-
-	public void removeTypeChangeListener(TypeChangeListener listener)
-	{
-		listeners.remove(listener);
-	}
-
-	@Override
-	public String getValueAsString()
-	{
-		Object value = getValue();
-
-		if (value == null) return "null";
-
-		if (value.getClass().isArray())
-		{
-			int length = Array.getLength(value);
-
-			if (length == 0) return "";
-
-			StringBuilder sb = new StringBuilder();
-
-			sb.append(Array.get(value, 0).toString());
-			for (int i = 1; i < length; i++)
-				sb.append(" " + Array.get(value, i).toString());
-
-			return sb.toString();
-		}
-
-		return value.toString();
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public boolean isAssignableFrom(Var source)
-	{
-		Class<?> sourceType = source.getType();
-
-		return sourceType != null && (this.getType() == null || this.getType().isAssignableFrom(sourceType));
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public void setDefaultEditorModel(VarEditorModel model)
-	{
-		// this is just to avoid the warning in client code
-		super.setDefaultEditorModel(model);
-	}
-
-	@SuppressWarnings("unchecked")
-	public void setType(Class<?> newType)
-	{
-		Class<?> oldType = this.getType();
-
-		if (oldType == newType) return;
-
-		setValue(null);
-
-		this.type = newType;
-
-		for (TypeChangeListener listener : listeners)
-			listener.typeChanged(this, oldType, newType);
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public void setValue(Object newValue)
-	{
-		super.setValue(newValue);
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public void setReference(Var variable) throws ClassCastException
-	{
-		// change the type just in time before setting the reference
-		if (variable != null) setType(variable.getType());
-		super.setReference(variable);
-	}
+    private final ArrayList<TypeChangeListener> listeners = new ArrayList<TypeChangeListener>();
+    
+    /**
+     * Constructs a new mutable variable with specified name and type. The default value for mutable
+     * variables is always null, in order to be reset properly
+     * 
+     * @param name
+     * @param initialType
+     *            the type of variable to handle (this can be changed via the
+     *            {@link #setType(Class)} method
+     */
+    @SuppressWarnings("unchecked")
+    public VarMutable(String name, Class<?> initialType)
+    {
+        super(name, initialType, null);
+    }
+    
+    @Override
+    public VarEditor createVarEditor()
+    {
+        if (getDefaultEditorModel() instanceof TypeSelectionModel) return new TypeChooser(this);
+        
+        return super.createVarEditor();
+    }
+    
+    public void addTypeChangeListener(TypeChangeListener listener)
+    {
+        listeners.add(listener);
+    }
+    
+    public void removeTypeChangeListener(TypeChangeListener listener)
+    {
+        listeners.remove(listener);
+    }
+    
+    @Override
+    public String getValueAsString()
+    {
+        Object value = getValue();
+        
+        if (value == null) return "null";
+        
+        if (value.getClass().isArray())
+        {
+            int length = Array.getLength(value);
+            
+            if (length == 0) return "";
+            
+            StringBuilder sb = new StringBuilder();
+            
+            sb.append(Array.get(value, 0).toString());
+            for (int i = 1; i < length; i++)
+                sb.append(" " + Array.get(value, i).toString());
+            
+            return sb.toString();
+        }
+        
+        return value.toString();
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean isAssignableFrom(Var source)
+    {
+        Class<?> sourceType = source.getType();
+        
+        return sourceType != null && (this.getType() == null || this.getType().isAssignableFrom(sourceType));
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public void setDefaultEditorModel(VarEditorModel model)
+    {
+        // this is just to avoid the warning in client code
+        super.setDefaultEditorModel(model);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public void setType(Class<?> newType)
+    {
+        if (isReferenced()) throw new IllegalAccessError("Cannot change the type of a linked mutable variable");
+        
+        Class<?> oldType = this.getType();
+        
+        if (oldType == newType) return;
+        
+        setValue(null);
+        
+        this.type = newType;
+        
+        for (TypeChangeListener listener : listeners)
+            listener.typeChanged(this, oldType, newType);
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public void setValue(Object newValue)
+    {
+        super.setValue(newValue);
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public void setReference(Var variable) throws ClassCastException
+    {
+        // change the type just in time before setting the reference
+        if (variable != null) setType(variable.getType());
+        super.setReference(variable);
+    }
 }
