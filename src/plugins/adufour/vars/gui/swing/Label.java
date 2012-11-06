4a91c0fa940bd8c0f5cadb91beb9696b713b9bfb
Alexandre Dufour <adufour@pasteur.fr>
2012-11-06 17:58:39 +0100

    Fix default maximum width of Label<T>

--- src/plugins/adufour/vars/gui/swing/Label.java
+++ src/plugins/adufour/vars/gui/swing/Label.java
@@ -1,5 +1,7 @@
 package plugins.adufour.vars.gui.swing;
 
+import java.awt.Dimension;
+
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 
@@ -7,31 +9,41 @@ import plugins.adufour.vars.lang.Var;
 
 public class Label<T> extends SwingVarEditor<T>
 {
+    public final int MAX_WIDTH = 150;
+    
     public Label(Var<T> variable)
     {
         super(variable);
     }
-
+    
     @Override
     protected JComponent createEditorComponent()
     {
         return new JLabel("");
     }
-
+    
     @Override
     protected void activateListeners()
     {
     }
-
+    
     @Override
     protected void deactivateListeners()
     {
     }
-
+    
     @Override
     public JLabel getEditorComponent()
     {
         return (JLabel) super.getEditorComponent();
+    }
+    
+    @Override
+    public Dimension getPreferredSize()
+    {
+        Dimension dim = getEditorComponent().getPreferredSize();
+        if (dim.width > MAX_WIDTH) dim.width = MAX_WIDTH;
+        return dim;
     }
     
     @Override
