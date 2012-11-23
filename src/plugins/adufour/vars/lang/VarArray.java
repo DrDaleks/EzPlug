8251b1307a4c4e474f2357151c966ac07636a26c
Alexandre Dufour <adufour@pasteur.fr>
2012-11-23 19:22:01 +0100

    * VarArray no longer deprecated (has a use after all !)

--- src/plugins/adufour/vars/lang/VarArray.java
+++ src/plugins/adufour/vars/lang/VarArray.java
@@ -1,11 +1,16 @@
 package plugins.adufour.vars.lang;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Convenience class defining a generic array of elements based on the specified inner type.<br/>
  * The type of the underlying array cannot be changed. To creates arrays of changeable types, use
- * {@link VarMutableArray} instead.
+ * {@link VarMutableArray} instead.<br/>
+ * NOTE: this class provides convenience methods to add elements (similarly to the {@link ArrayList}
+ * class, however performance on large arrays is not optimal. For performance-critical applications,
+ * use the {@link VarArray} instead
  * 
- * @deprecated use {@link VarGenericArray} instead
  * @author Alexandre Dufour
  * 
  * @param <T>
@@ -13,8 +18,38 @@ package plugins.adufour.vars.lang;
  */
 public class VarArray<T> extends VarGenericArray<T[]>
 {
+    /**
+     * Creates a new array variable
+     * 
+     * @param name
+     *            the variable name
+     * @param type
+     *            the data type of the array (including the <code>[]</code>)
+     * @param defaultValue
+     *            the initial array
+     */
     public VarArray(String name, Class<T[]> type, T[] defaultValue)
     {
         super(name, type, defaultValue);
+    }
+    
+    /**
+     * Inserts the specified elements at the end of this array. This methods acts similarly to
+     * {@link ArrayList#add(Object)}: a old array is replaced by a new array where the contents of
+     * the old array is copied and the specified element is added last
+     * 
+     * @param elements
+     *            the elements to add
+     */
+    public void add(T... elements)
+    {
+        T[] oldArray = getValue();
+        ArrayList<T> newArray = new ArrayList<T>(size() + elements.length);
+        
+        for (T oldElem : oldArray)
+            newArray.add(oldElem);
+        newArray.addAll(Arrays.asList(elements));
+        
+        setValue(newArray.toArray(oldArray));
     }
 }
