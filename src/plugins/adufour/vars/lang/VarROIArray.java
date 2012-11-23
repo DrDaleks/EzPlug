a81e166b8e40611575921b6c5d641ed341f8777b
Alexandre Dufour <adufour@pasteur.fr>
2012-11-23 19:22:18 +0100

    VarROIArray extends VarArray

--- src/plugins/adufour/vars/lang/VarROIArray.java
+++ src/plugins/adufour/vars/lang/VarROIArray.java
@@ -2,10 +2,10 @@ package plugins.adufour.vars.lang;
 
 import icy.roi.ROI;
 
-public class VarROIArray extends VarGenericArray<ROI[]>
+public class VarROIArray extends VarArray<ROI>
 {
     public VarROIArray(String name)
     {
-        super(name, ROI[].class, null);
+        super(name, ROI[].class, new ROI[0]);
     }
 }
