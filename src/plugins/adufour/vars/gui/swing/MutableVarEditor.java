826f9fde7cf79670912f1abf5d73403a674efa75
Alexandre Dufour <adufour@pasteur.fr>
2012-12-07 17:37:11 +0100

    Removed Workbook related vars (should come with POI)

--- src/plugins/adufour/vars/gui/swing/MutableVarEditor.java
+++ src/plugins/adufour/vars/gui/swing/MutableVarEditor.java
@@ -7,12 +7,9 @@ import java.awt.Dimension;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 
-import org.apache.poi.ss.usermodel.Workbook;
-
 import plugins.adufour.vars.gui.VarEditor;
 import plugins.adufour.vars.lang.VarMutable;
 import plugins.adufour.vars.lang.VarSequence;
-import plugins.adufour.vars.lang.VarWorkbook;
 
 /**
  * Special editor that changes type according to the underlying variable
@@ -37,11 +34,6 @@ public class MutableVarEditor extends SwingVarEditor<Object>
      * Editor used if the inner type of the variable is compatible with {@link VarSequence}
      */
     private SequenceViewer sequenceViewer;
-    
-    /**
-     * Editor used if the inner type of the variable is compatible with {@link VarWorkbook}
-     */
-    private WorkbookEditor workbookEditor;
     
     /**
      * Editor used for any type that misses a dedicted editor
@@ -85,10 +77,6 @@ public class MutableVarEditor extends SwingVarEditor<Object>
                 sequenceViewer = new SequenceViewer((VarMutable) variable);
             }
             varEditor = sequenceViewer;
-        }
-        else if (variable.getType() != null && variable.getType().isAssignableFrom(Workbook.class))
-        {
-            if (workbookEditor == null) workbookEditor = new WorkbookEditor((VarMutable) variable);
         }
         else
         {
