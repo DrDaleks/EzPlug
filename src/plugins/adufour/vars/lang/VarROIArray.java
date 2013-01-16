dd47af41bb08675b24612040867b452b26a77017
Alexandre Dufour <adufour@pasteur.fr>
2013-01-16 14:50:41 +0100

    VarROIArray overrides Var.getValueAsString() to be more user friendly

--- src/plugins/adufour/vars/lang/VarROIArray.java
+++ src/plugins/adufour/vars/lang/VarROIArray.java
@@ -8,4 +8,20 @@ public class VarROIArray extends VarArray<ROI>
     {
         super(name, ROI[].class, new ROI[0]);
     }
+    
+    /**
+     * @return a pretty-printed text representation of the variable's local value (referenced
+     *         variables are <b>not</b> followed). This text is used to display the value (e.g. in a
+     *         graphical interface) or store the value into XML files. Overriding implementations
+     *         should make sure that the result of this method is compatible with the
+     *         {@link #parse(String)} method to ensure proper reloading from XML files.
+     */
+    public String getValueAsString()
+    {
+        ROI[] value = getValue();
+        
+        if (value == null || value.length == 0) return "No ROI";
+        
+        return value.length + " ROI";
+    }
 }
