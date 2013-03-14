a526f3ee3a657b4ef58617334bb28c6900466a62
Alexandre Dufour <adufour@pasteur.fr>
2013-03-14 18:18:35 +0100

    TypeSelectionModel: Replace null by Object for "anything"

--- src/plugins/adufour/vars/gui/model/TypeSelectionModel.java
+++ src/plugins/adufour/vars/gui/model/TypeSelectionModel.java
@@ -11,7 +11,7 @@ public class TypeSelectionModel extends ValueSelectionModel<Class<?>>
 	 */
 	public TypeSelectionModel()
 	{
-		super(new Class<?>[] { null, Sequence.class, Integer.class, Double.class, int[].class, double[].class, String.class, File.class, File[].class });
+		super(new Class<?>[] { Object.class, Sequence.class, Integer.class, Double.class, int[].class, double[].class, String.class, File.class, File[].class });
 	}
 
 	/**
