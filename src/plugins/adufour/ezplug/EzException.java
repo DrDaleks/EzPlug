71a655426abca7ee7aab930789f7cf53ba090e82
Alexandre Dufour <adufour@pasteur.fr>
2014-07-22 17:26:54 +0100

    Improved exception classes to store source objects

--- src/plugins/adufour/ezplug/EzException.java
+++ src/plugins/adufour/ezplug/EzException.java
@@ -1,23 +1,46 @@
 package plugins.adufour.ezplug;
 
+@SuppressWarnings("serial")
 public class EzException extends RuntimeException
 {
-	private static final long	serialVersionUID	= 666L;
-	
-	public final boolean		catchException;
-	
-	/**
-	 * Creates a new instance of EzException with the specified message and catching behavior.
-	 * 
-	 * @param message
-	 *            the error message bound the current exception
-	 * @param catchException
-	 *            true if the exception should be caught within the EzPlug layer, false to let the
-	 *            exception pass to the global exception manager
-	 */
-	public EzException(String message, boolean catchException)
-	{
-		super(message);
-		this.catchException = catchException;
-	}
+    /**
+     * The plug-in where the exception occurred
+     */
+    public final EzPlug  source;
+    
+    public final boolean catchException;
+    
+    /**
+     * Creates a new instance of EzException with the specified message and catching behavior.
+     * 
+     * @deprecated use {@link #EzException(EzPlug, String, boolean)} to specify the source of this
+     *             exception
+     * @param message
+     *            the error message bound the current exception
+     * @param catchException
+     *            true if the exception should be caught within the EzPlug layer, false to let the
+     *            exception pass to the global exception manager
+     */
+    public EzException(String message, boolean catchException)
+    {
+        this(null, message, catchException);
+    }
+    
+    /**
+     * Creates a new instance of EzException with the specified message and catching behavior.
+     * 
+     * @param source
+     *            The plug-in where the exception occurred
+     * @param message
+     *            the error message bound the current exception
+     * @param catchException
+     *            true if the exception should be caught within the EzPlug layer, false to let the
+     *            exception pass to the global exception manager
+     */
+    public EzException(EzPlug source, String message, boolean catchException)
+    {
+        super(message);
+        this.catchException = catchException;
+        this.source = source;
+    }
 }
--- src/plugins/adufour/vars/util/VarException.java
+++ src/plugins/adufour/vars/util/VarException.java
@@ -1,22 +1,47 @@
 package plugins.adufour.vars.util;
 
+import plugins.adufour.ezplug.EzVar;
+import plugins.adufour.vars.lang.Var;
+
 /**
  * Utility class used to notify users that a variable value is invalid
  * 
  * @author Alexandre Dufour
- * 
  */
 @SuppressWarnings("serial")
 public class VarException extends IllegalArgumentException
 {
     /**
+     * The variable causing the exception
+     */
+    public final Var<?> source;
+    
+    /**
      * Creates a new exception with the specified message
      * 
+     * @deprecated use {@link #VarException(Var, String)} to specify the source of this exception
      * @param message
      *            the error message behind this exception
      */
     public VarException(String message)
     {
+        this(null, message);
+    }
+    
+    /**
+     * Creates a new exception with the specified message
+     * 
+     * @param source
+     *            the variable causing the exception (will be used to provide better user experience
+     *            when displaying error messages). If the source of the exception is an
+     *            {@link EzVar} object, indicate here its underlying variable, accessible via
+     *            {@link EzVar#getVariable()}
+     * @param message
+     *            the error message behind this exception
+     */
+    public VarException(Var<?> source, String message)
+    {
         super(message);
+        this.source = source;
     }
 }
