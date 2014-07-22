d922639e965aab332d55d21f6578f8c81342d879
Alexandre Dufour <adufour@pasteur.fr>
2014-07-22 17:26:14 +0100

    Removed unused try/catch block

--- src/plugins/adufour/ezplug/EzVarPlugin.java
+++ src/plugins/adufour/ezplug/EzVarPlugin.java
@@ -3,13 +3,11 @@ package plugins.adufour.ezplug;
 import icy.plugin.PluginDescriptor;
 import icy.plugin.abstract_.Plugin;
 import plugins.adufour.vars.lang.VarPlugin;
-import plugins.adufour.vars.util.VarException;
 
 /**
  * New variable displaying a list of plug-ins of a given type
  * 
  * @author Alexandre Dufour
- * 
  * @param <P>
  */
 public class EzVarPlugin<P extends Plugin> extends EzVar<PluginDescriptor>
@@ -22,13 +20,6 @@ public class EzVarPlugin<P extends Plugin> extends EzVar<PluginDescriptor>
     @SuppressWarnings("unchecked")
     public P newInstance()
     {
-        try
-        {
-            return ((VarPlugin<P>) variable).newInstance();
-        }
-        catch (VarException e)
-        {
-            throw new EzException(e.getMessage(), true);
-        }
+        return ((VarPlugin<P>) variable).newInstance();
     }
 }
