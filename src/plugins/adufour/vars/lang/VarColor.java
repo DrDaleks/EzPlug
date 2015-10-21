a9c5644c8b8b22ca8f444d6c0c1415786ef84ed9
Alexandre Dufour <adufour@pasteur.fr>
2015-10-22 00:34:44 +0200

    VarColor are now properly reloaded (e.g. from protocol files)

--- src/plugins/adufour/vars/lang/VarColor.java
+++ src/plugins/adufour/vars/lang/VarColor.java
@@ -2,6 +2,8 @@ package plugins.adufour.vars.lang;
 
 import java.awt.Color;
 
+import org.w3c.dom.Node;
+
 import plugins.adufour.vars.gui.VarEditor;
 import plugins.adufour.vars.gui.VarEditorFactory;
 import plugins.adufour.vars.util.VarListener;
@@ -36,5 +38,26 @@ public class VarColor extends Var<Color>
     public VarEditor<Color> createVarEditor()
     {
         return VarEditorFactory.getDefaultFactory().createColorChooser(this);
+    }
+    
+    @Override
+    public boolean saveToXML(Node node) throws UnsupportedOperationException
+    {
+        return super.saveToXML(node);
+    }
+    
+    @Override
+    public Color parse(String text)
+    {
+        if (text == null) return Color.black;
+        
+        // text should look like:
+        // java.awt.Color[r=0,g=255,b=255]
+        text = text.substring(text.indexOf('[') + 1, text.indexOf(']'));
+        String[] rgb = text.split(",");
+        int r = Integer.parseInt(rgb[0].substring(rgb[0].indexOf('=') + 1));
+        int g = Integer.parseInt(rgb[1].substring(rgb[1].indexOf('=') + 1));
+        int b = Integer.parseInt(rgb[2].substring(rgb[2].indexOf('=') + 1));
+        return new Color(r, g, b);
     }
 }
