287a939b32dafb1302bb185f2583c7b1e8b32f2f
Alexandre Dufour <adufour@pasteur.fr>
2017-02-24 16:48:53 +0100

    New: EzVarFile now has new constructors to support file filters (wildcards or regex)

--- src/plugins/adufour/ezplug/EzVarFile.java
+++ src/plugins/adufour/ezplug/EzVarFile.java
@@ -1,17 +1,20 @@
 package plugins.adufour.ezplug;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.util.regex.Pattern;
 
+import icy.util.StringUtil;
 import plugins.adufour.vars.gui.FileMode;
 import plugins.adufour.vars.gui.model.FileTypeModel;
 import plugins.adufour.vars.gui.swing.FileChooser;
 import plugins.adufour.vars.lang.VarFile;
 
 /**
- * Class defining a variable of type String, embarking a button triggering a file dialog as graphical component
+ * Class defining a variable of type String, embarking a button triggering a file dialog as
+ * graphical component
  * 
  * @author Alexandre Dufour
- * 
  */
 public class EzVarFile extends EzVar<File>
 {
@@ -25,9 +28,63 @@ public class EzVarFile extends EzVar<File>
      */
     public EzVarFile(String varName, String path)
     {
-        super(new VarFile(varName, null), new FileTypeModel(path, FileMode.FILES, null, false));
+        this(varName, path, (FileFilter) null);
     }
-
+    
+    /**
+     * Constructs a new input variable with given name, file path and filter
+     * 
+     * @param varName
+     *            the name of the variable (as it will appear in the interface)
+     * @param path
+     *            the default path to show in the file dialog
+     * @param wildcard
+     *            a wild-card filter (e.g. "*.tif"). Warning: this parameter is *not* a regular
+     *            expression. To use regular expressions, see
+     *            {@link #EzVarFile(String, String, Pattern)}
+     */
+    public EzVarFile(String varName, String path, final String wildcard)
+    {
+        this(varName, null, Pattern.compile(StringUtil.wildcardToRegex(wildcard)));
+    }
+    
+    /**
+     * Constructs a new input variable with given name, file path and filter
+     * 
+     * @param varName
+     *            the name of the variable (as it will appear in the interface)
+     * @param path
+     *            the default path to show in the file dialog
+     * @param pattern
+     *            a regular expression pattern to filter the file list
+     */
+    public EzVarFile(String varName, String path, final Pattern pattern)
+    {
+        this(varName, null, new FileFilter()
+        {
+            @Override
+            public boolean accept(File pathname)
+            {
+                return pathname.isDirectory() || Pattern.matches(pattern.pattern(), pathname.getPath());
+            }
+        });
+    }
+    
+    /**
+     * Constructs a new input variable with given name, file path and filter
+     * 
+     * @param varName
+     *            the name of the variable (as it will appear in the interface)
+     * @param path
+     *            the default path to show in the file dialog
+     * @param filter
+     *            the file filter
+     */
+    public EzVarFile(String varName, String path, FileFilter filter)
+    {
+        super(new VarFile(varName, null), new FileTypeModel(path, FileMode.FILES, filter, false));
+    }
+    
     /**
      * Replaces the button text by the given string
      * 
--- src/plugins/adufour/vars/gui/swing/FileChooser.java
+++ src/plugins/adufour/vars/gui/swing/FileChooser.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileFilter;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -20,12 +21,11 @@ import plugins.adufour.vars.lang.Var;
  * File chooser component
  * 
  * @author Alexandre Dufour
- * 
  */
 public class FileChooser extends SwingVarEditor<File>
 {
     private ActionListener actionListener;
-
+    
     /**
      * Creates a new file chooser component
      * 
@@ -36,68 +36,88 @@ public class FileChooser extends SwingVarEditor<File>
     {
         super(variable);
     }
-
+    
     @Override
     public JComponent createEditorComponent()
     {
         JButton jButton = new JButton();
-
+        
         String path = null;
         FileMode fileMode = FileMode.ALL;
         boolean allowHidden = false;
-
+        final FileFilter filter;
+        
         VarEditorModel<File> model = variable.getDefaultEditorModel();
-
+        
         if (model instanceof FileTypeModel)
         {
             path = ((FileTypeModel) model).getPath();
             fileMode = ((FileTypeModel) model).getMode();
             allowHidden = ((FileTypeModel) model).allowHidden();
+            filter = ((FileTypeModel) model).getFilter();
         }
-
+        else
+        {
+            filter = null;
+        }
+        
         final JFileChooser jFileChooser = new JFileChooser(path);
         switch (fileMode)
         {
-        case FILES:
-            jFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            jButton.setText("Choose file name...");
-        break;
-
-        case FOLDERS:
-            jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            jButton.setText("Choose folder name...");
-        break;
-
-        default:
-            jFileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-            jButton.setText("Choose file or folder name...");
+            case FILES:
+                jFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                jButton.setText("Choose file name...");
+            break;
+        
+            case FOLDERS:
+                jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                jButton.setText("Choose folder name...");
+            break;
+        
+            default:
+                jFileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                jButton.setText("Choose file or folder name...");
         }
-
+        
         jFileChooser.setMultiSelectionEnabled(false);
         jFileChooser.setFileHidingEnabled(!allowHidden);
-
+        if (filter != null) jFileChooser.setFileFilter(new javax.swing.filechooser.FileFilter()
+        {
+            @Override
+            public String getDescription()
+            {
+                return "Compatible files";
+            }
+            
+            @Override
+            public boolean accept(File f)
+            {
+                return filter.accept(f);
+            }
+        });
+        
         actionListener = new ActionListener()
         {
             public void actionPerformed(ActionEvent e)
             {
                 if (jFileChooser.showOpenDialog(null) != JFileChooser.APPROVE_OPTION) return;
-
+                
                 variable.setValue(jFileChooser.getSelectedFile());
                 setButtonText(jFileChooser.getSelectedFile().getAbsolutePath());
             }
         };
-
+        
         // jButton.addActionListener(actionListener);
-
+        
         return jButton;
     }
-
+    
     @Override
     public Dimension getPreferredSize()
     {
         return new Dimension(100, 20);
     }
-
+    
     /**
      * Replaces the button text by the given string
      * 
@@ -105,7 +125,7 @@ public class FileChooser extends SwingVarEditor<File>
      */
     public void setButtonText(final String text)
     {
-
+        
         ThreadUtil.invokeLater(new Runnable()
         {
             public void run()
@@ -114,37 +134,37 @@ public class FileChooser extends SwingVarEditor<File>
             }
         });
     }
-
+    
     @Override
     protected void updateInterfaceValue()
     {
         File newValue = variable.getValue();
-
+        
         String tooltip = "<html><pre><font size=3>";
-
+        
         if (newValue != null)
         {
             tooltip += newValue.getAbsolutePath();
             if (newValue.isDirectory()) tooltip += "/";
         }
-
+        
         tooltip += "</font></pre></html>";
-
+        
         getEditorComponent().setToolTipText(tooltip);
     }
-
+    
     @Override
     public JButton getEditorComponent()
     {
         return (JButton) super.getEditorComponent();
     }
-
+    
     @Override
     protected void activateListeners()
     {
         getEditorComponent().addActionListener(actionListener);
     }
-
+    
     @Override
     protected void deactivateListeners()
     {
